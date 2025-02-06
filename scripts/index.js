33d11058cd6759c1fa6512640b606d293084749c
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-02-06 15:43:08 -0800

    sprint 4 project 1 part 1 commit 1

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    name: "Joseph, OR",
+    link: "https://unsplash.com/photos/a-view-of-a-mountain-range-with-a-lake-in-the-foreground-hus3TC-C3RQ",
+  },
+  {
+    name: "Oregon Coast",
+    link: "https://unsplash.com/photos/black-rock-formation-on-body-of-water-during-daytime-IdEV3S3JkyU",
+  },
+  {
+    name: "Tumalo Falls, OR",
+    link: "https://unsplash.com/photos/green-trees-near-waterfalls-during-daytime-sbH8xclbcTY",
+  },
+  {
+    name: "Central Oregon Cascade Range",
+    link: "https://unsplash.com/photos/brown-mountains-7kh9KKPWBaQ",
+  },
+  {
+    name: "Multnomah Falls, OR",
+    link: "https://unsplash.com/photos/waterfall-in-forest-jQ5mIxOgzBE",
+  },
+  {
+    name: "Southern Oregon",
+    link: "https://unsplash.com/photos/the-sun-is-shining-through-the-trees-in-the-field-EqOOC2-0qjc",
+  },
+];
+
+console.log(initialCards);
