f24f5ff08796ce36f6af0cf23eee59c208a192c2
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-02-17 22:39:23 -0800

    sprint 4 project part 4 commit 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,37 @@
+// -------------------------OPEN/CLOSE MODULE----------------------------------
+
+const profileEditButton = document.querySelector(".profile__edit-btn");
+const modalOpen = document.querySelector(".modal");
+const modalCloseButton = document.querySelector(".modal__button-close");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const editModalNameInput = document.querySelector("#profile-name-input");
+const editModalDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const editFormElement = document.querySelector(".modal__form");
+
+function openModel() {
+  editModalNameInput.value = profileName.textContent;
+  editModalDescriptionInput.value = profileDescription.textContent;
+  modalOpen.classList.add("modal_opened");
+}
+
+function closeModel() {
+  modalOpen.classList.remove("modal_opened");
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = editModalNameInput.value;
+  profileDescription.textContent = editModalDescriptionInput.value;
+  closeModel();
+}
+
+profileEditButton.addEventListener("click", openModel);
+modalCloseButton.addEventListener("click", closeModel);
+editFormElement.addEventListener("submit", handleProfileFormSubmit);
+
 // ------------------------------ARRAY----------------------------------
 
 const initialCards = [
@@ -28,23 +62,25 @@ const initialCards = [
 ];
 console.log(initialCards);
 
-// -------------------------------OPEN----------------------------------
+// --------------------------CARD TEMPLATES-------------------------------
 
-const profileEditButton = document.querySelector(".profile__edit-btn");
-const modalOpen = document.querySelector(".modal");
+const cardTemplate = document.querySelector("#card-template");
+const cardList = document.querySelector(".cards__list");
 
-function openModel() {
-  modalOpen.classList.add("modal_opened");
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameEl = cardElement.querySelector(".card__title");
+  const imageElement = cardTemplate.querySelector(".card__image");
+
+  cardNameEl.textContent = data.name;
+
+  return cardElement;
 }
 
-profileEditButton.addEventListener("click", openModel);
-
-// -------------------------------CLOSE------------------------------------------
-
-const modalCloseButton = document.querySelector(".modal__button-close");
-
-function closeModel() {
-  modalOpen.classList.remove("modal_opened");
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards(i));
+  cardslist.prepend(cardElement);
 }
-
-modalCloseButton.addEventListener("click", closeModel);
