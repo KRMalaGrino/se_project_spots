bc076914a2d70eba66f88bf9f134203f95724c4d
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-03-09 17:03:27 -0700

    feat: added error message html & css

---
+++ scripts/validation.js
@@ -0,0 +1,14 @@
+// ------------------ Variables -----------------
+
+const modal = document.querySelectorAll(".modal");
+const modalContainer = document.querySelectorAll(".modal__container");
+const modalForm = document.querySelectorAll(".modal__form");
+const closeButton = document.querySelectorAll(".modal__button-close");
+const label = document.querySelectorAll(".modal__label");
+const input = document.querySelectorAll(".modal__input");
+
+// ------------------ functions ------------------
+
+// const enableValidation = () => {};
+
+// ------------------ Event Listeners -----------------
